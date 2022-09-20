181adf35db278c279ad324be5062e467a9c39432
Srivignesh Sivakumar <shreez77890@gmail.com>
2022-09-19 22:51:33 -0500

    APIs Exercise

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "1QhgR8EoKiIzjwLtGRBWRDy5f3Qyxx0Z";
+
+
+
+
+let feedbackEle = document.querySelector("#feedback");
+let searchInput = document.querySelector("#searchWord");
+let searchBtn = document.querySelector("#submitSearch");
+let gifEle = document.querySelector("#imageContainer > img");
+
+searchBtn.addEventListener("click", (event) =>{
+getGif(searchInput.value);
+});
+
+function getGif(searchTerm) {
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchInput.value}`)
+    .then((res) => res.json())
+    .then((body) => {
+        gifEle.src = body.data.images.original.url;
+    })
+    .catch((err) => { console.error(err);
+
+    feedbackEle.textContent = err.message;
+    });   
+}
+
